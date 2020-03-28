ac7ca30b4f6761e3a6cc626cd7116494b9a0c1d8
Peter Shodeinde <petershodeinde4@gmail.com>
2020-03-28 20:02:26 +0200

    added the landing page to watch the church service

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { graphql, withPrefix, Link } from 'gatsby';
 import Helmet from 'react-helmet';
+import Iframe from 'react-iframe';
 import SEO from '../components/SEO';
 import Layout from '../layouts/index';
 import Call from '../components/Call';
@@ -17,63 +18,20 @@ const Home = (props) => {
           content="Small Business Theme. Multiple content types using Markdown and JSON sources. Responsive design and SCSS. This is a beautiful and artfully designed starting theme."
         />
       </Helmet>
-      <div className="intro pb-4">
+      <div className="pb-4" style={{ paddingTop: 50 }}>
         <div className="container">
-          <h1>Serif - Gatsby Small Business Theme.</h1>
-          <p>
-            Multiple content types using Markdown and JSON sources. Responsive design and SCSS. This
-            is a beautiful and artfully designed starting theme.
-          </p>
-        </div>
-      </div>
-
-      <div className="container pt-2">
-        <Call button />
-      </div>
-
-      <div className="container pt-8 pt-md-10">
-        <div className="row justify-content-start">
-          <div className="col-12">
-            <h2 className="title-3 text-dark mb-3">Our Services</h2>
-          </div>
-          {markdown.map(edge => (
-            <div key={edge.node.frontmatter.path} className="col-12 col-md-4 mb-1">
-              <div className="card service service-teaser">
-                <div className="card-content">
-                  <h2>
-                    <Link to={edge.node.frontmatter.path}>{edge.node.frontmatter.title}</Link>
-                  </h2>
-                  <p>{edge.node.excerpt}</p>
-                </div>
-              </div>
-            </div>
-          ))}
-          <div className="col-12 text-center">
-            <Link className="button button-primary mt-2" to="/services">
-              View All Services
-            </Link>
-          </div>
-        </div>
-      </div>
-
-      <div className="container pt-5 pb-5 pt-md-7 pb-md-7">
-        <div className="row justify-content-center">
-          <div className="col-12">
-            <h2 className="title-3 text-dark mb-4">Our Features</h2>
-          </div>
-          {json.map(edge => (
-            <div key={edge.node.id} className="col-12 col-md-6 col-lg-4 mb-2">
-              <div className="feature">
-                {edge.node.image && (
-                  <div className="feature-image">
-                    <img src={withPrefix(edge.node.image)} />
-                  </div>
-                )}
-                <h2 className="feature-title">{edge.node.title}</h2>
-                <div className="feature-content">{edge.node.description}</div>
-              </div>
-            </div>
-          ))}
+          <h1>Watch Sunday service</h1>
+          <Iframe
+            url="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2F710398912312558%2Fvideos%2F149051939770632%2F&show_text=1&width=560"
+            width="1000px"
+            height="600px"
+            id="myId"
+            allowTransparency="true"
+            allowFullScreen="true"
+            className="myClassname"
+            display="initial"
+            position="relative"
+          />
         </div>
       </div>
     </Layout>
