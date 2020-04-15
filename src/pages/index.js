5319c32d236221912ee263b357c1ca72aeaed47e
Peter Shodeinde <petershodeinde4@gmail.com>
2020-04-15 18:06:50 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,9 +20,9 @@ const Home = (props) => {
       </Helmet>
       <div className="pb-4" style={{ paddingTop: 50 }}>
         <div className="container">
-          <h1>Watch Sunday service live</h1>
+          <h1>Watch Bible Study live</h1>
           <Iframe
-            url="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Frccghelsinkifi%2Fvideos%2F659589761507941%2F&width=1280"
+            url="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Frccghelsinkifi%2Fvideos%2F267379560951813%2F&width=1280"
             width="1000px"
             height="600px"
             id="myId"
