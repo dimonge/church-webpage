49538a11d38d6bfb6b4dcfb86f8046baca58e2e7
Peter Shodeinde <petershodeinde4@gmail.com>
2020-07-01 18:42:19 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,7 +22,7 @@ const Home = (props) => {
         <div className="container">
           <h1>Watch Bible study live</h1>
           <Iframe
-            url="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Frccghelsinkifi%2Fvideos%2F2621189271485140%2F&width=1280"
+            url="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Frccghelsinkifi%2Fvideos%2F1463563883832029%2F&width=1280"
             width="1000px"
             height="600px"
             id="myId"
