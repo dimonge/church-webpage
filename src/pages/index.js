5484b8a96ee15fd99f560627b0aa5f4114e6b6cf
Peter Shodeinde <petershodeinde4@gmail.com>
2020-07-19 12:20:29 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,6 +33,8 @@ const Home = (props) => {
             display="initial"
             position="relative"
           />
+    
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/CUi_dw4FAlc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
       </div>
     </Layout>
