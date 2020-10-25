64d06db082d844f26bba3f024c091d20bf269290
Peter Shodeinde <petershodeinde4@gmail.com>
2020-10-25 12:09:45 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,7 +24,7 @@ const Home = (props) => {
          <iframe 
           width="1000"
           height="600"
-          src="https://www.youtube.com/embed/twdUlzO5ErU" 
+          src="https://www.youtube.com/embed/4jUd2OUVEK0" 
           frameborder="0" 
           allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
           allowfullscreen></iframe> 
