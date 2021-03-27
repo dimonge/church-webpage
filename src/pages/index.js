2f036c26413932ad1af1e5c60450e5cffbed9209
Peter Shodeinde <petershodeinde4@gmail.com>
2021-03-27 19:02:04 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ const Home = (props) => {
       </Helmet>
       <div className="pb-4" style={{ paddingTop: 50 }}>
         <div className="container">
-          <h1>Watch Sunday service live</h1>
+          <h1>Watch Change for Success 2021 live</h1>
          <iframe 
           width="1000"
           height="600"
